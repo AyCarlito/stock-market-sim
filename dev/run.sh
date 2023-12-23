b7f99517d015048e725bbedc921005e338146a4b
Mustafaa <mustafaa.ahmad@hotmail.co.uk>
2023-12-23 18:16:32 +0000

    Fix: Container user and group

--- dev/run.sh
+++ dev/run.sh
@@ -2,7 +2,5 @@
 
 GIT_ROOT="$(git rev-parse --show-toplevel)"
 
-
-
-docker run -it --entrypoint /bin/sh --rm --user 1000:1000 -p 127.0.0.1:3000:3000 -v ${GIT_ROOT}:/app stock-market-simulation-dev:0.0.1
+docker run -it --entrypoint /bin/sh --rm --user $(id -u):$(id -g) -p 127.0.0.1:3000:3000 -v ${GIT_ROOT}:/app stock-market-simulation-dev:0.0.1
 
