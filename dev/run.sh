ae3882bc72f6fe33a9e178ee82bb68cd27408892
Daniel Hislop <39573158+AyCarlito@users.noreply.github.com>
2023-12-23 18:39:03 +0000

    App scaffolding / devenv setup (#1)

---
+++ dev/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+GIT_ROOT="$(git rev-parse --show-toplevel)"
+
+docker run -it --entrypoint /bin/sh --rm --user $(id -u):$(id -g) -p 127.0.0.1:3000:3000 -v ${GIT_ROOT}:/app stock-market-simulation-dev:0.0.1
+
