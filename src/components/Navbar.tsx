1b5cfc9448d7bc627f447c0916ff66ab14695dcd
Daniel Hislop <39573158+AyCarlito@users.noreply.github.com>
2023-12-24 14:11:50 +0000

    feat: Navbar component (#2)
    This PR:
    - Add sticky top navbar component
    - Create src/components directory for react components
    - Remove redundant scaffolding files

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <nav className="top-sticky">
+            <p className="top-name">Stock Market Simulation</p>
+            <ul className="top-links">
+                <li className="top-link">Home</li>
+                <li className="top-link">About</li>
+                <li className="top-link">GitHub</li>
+            </ul>
+        </nav>
+    )
+}
